46a34e85567c84d3932baa939c8ce412d57da4c1
ictaMRU <126146897+ictaMRU@users.noreply.github.com>
2023-02-24 13:11:27 +0400

    Update customTemplateSample.ts

--- src/templates/samples/customTemplateSample.ts
+++ src/templates/samples/customTemplateSample.ts
@@ -1,28 +1,32 @@
 import { v2 } from "@govtechsg/open-attestation";
 
-export interface CustomTemplateCertificate extends v2.OpenAttestationDocument {
+export interface CooTemplateCertificate extends v2.OpenAttestationDocument {
   name: string;
   institute: string;
-  foo?: {
-    title: string;
+  recipient: {
+    name: string;
   };
-  $template: v2.TemplateObject;
 }
 
-export const customTemplateCertificate: CustomTemplateCertificate = {
-  name: "John Doe",
-  institute: "Institute of John Doe",
+export const cooTemplateCertificate: CooTemplateCertificate = {
+  name: "Certificate of Origin",
+  institute: "ICT Authority",
   issuers: [
     {
-      name: "institute of blockchain"
-    }
+      name: "Trilok Dabeesing",
+      documentStore: "0x00e905DEF4Ad06129A5b2075B52CC71429f4c448",
+      identityProof: {
+        location: "icta.mu",
+        type: v2.IdentityProofType.DNSTxt,
+      },
+    },
   ],
-  $template: {
-    name: "custom",
-    type: v2.TemplateType.EmbeddedRenderer,
-    url: "http://localhost:3000"
+  recipient: {
+    name: "MRA Officer",
   },
-  foo: {
-    title: "Bar is awesome"
-  }
+  $template: {
+    name: "COO",
+    type: v2.TemplateType.EmbeddedRenderer,
+    url: "http://localhost:3000",
+  },
 };
